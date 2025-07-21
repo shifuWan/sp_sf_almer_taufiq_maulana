da501888fefaa286974bb4c785fab9ae597be9bc
shifuWan <almertamal@gmail.com>
2025-07-21 11:09:48 +0700

    Wrap dashboard layout in SessionProvider to enable session management for authenticated users.

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,14 +1,17 @@
 import Header from "@/components/sections/header";
 import { Toaster } from "@/components/ui/sonner";
+import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="flex-1">
-                {children}
-            </main>
-            <Toaster position="top-right" richColors />
+            <SessionProvider>
+                <Header />
+                <main className="flex-1">
+                    {children}
+                </main>
+                <Toaster position="top-right" richColors />
+            </SessionProvider>
         </div>
     )
 }
